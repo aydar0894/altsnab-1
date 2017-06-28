155988c3be1507023b5150ecd15b69fc8b356fde
aydar0894 <aydar0894@mail.ru>
2017-06-28 20:41:47 +0300

    !PersonalAccount Registration

--- db/migrate/20170628143757_users_default_role.rb
+++ db/migrate/20170628143757_users_default_role.rb
@@ -1,5 +1,5 @@
 class UsersDefaultRole < ActiveRecord::Migration[5.0]
   def change
-  	change_column :users, :role, :text, :default => '1'
+  	change_column :users, :role, :text, :default => '0'
   end
 end

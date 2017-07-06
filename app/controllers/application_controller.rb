5ecbe825a8e29da055998022459315d0a1d6e39b
Anvar <anvarzkr@gmail.com>
2017-07-06 04:45:31 +0300

    ! fix decrement > 0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
   def add_item_to_cart(item)
     (session[:cart] ||= []) << {item_id: item.id, count: 1, additional_equipment: []}
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
 
   def decrement_item_count_in_cart(item_id)
     session[:cart] = session[:cart].each do |cart_item|
-      cart_item['count'] = cart_item['count'].to_i - 1 if cart_item['item_id'] == item_id.to_i
+      cart_item['count'] = cart_item['count'].to_i - 1 if cart_item['item_id'] == item_id.to_i && cart_item['count'].to_i > 1
     end
   end
 
